793d890d810f80c224211e665b9a2655cde2df40
josecast1 <136867149+josecast1@users.noreply.github.com>
2025-10-21 09:51:14 -0400

    feat: created milestone component (#7)

--- coe/src/app/layout.tsx
+++ coe/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,10 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const ibmPlex = IBM_Plex_Sans({
   subsets: ["latin"],
 });
 
@@ -24,11 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      {/* <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
+      </body> */}
+      <body className={ibmPlex.className}>{children}</body>
     </html>
   );
 }
