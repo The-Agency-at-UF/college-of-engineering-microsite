e51102cbf6f352edfeb3493f402ae90cd29e822c
Adora Lin <157913762+LeviGotBoom@users.noreply.github.com>
2025-11-04 15:30:52 -0500

    feat: minimal bug fixes

--- coe/src/app/components/EventGridComponent.tsx
+++ coe/src/app/components/EventGridComponent.tsx
@@ -3,12 +3,8 @@ import EventCardComponent from "./EventCardComponent";
 
 const EventGridComponent = () => {
   return (
-    <section className="w-full py-10 px-8 bg-white">
-      {/* Removed the top “MORE FROM XXXX” label */}
-      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
-        <EventCardComponent />
-        <EventCardComponent />
-        <EventCardComponent />
+    <section className="w-full py-20 px-8 bg-white"> {/* increased top padding */}
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 place-items-center pt-10"> 
         <EventCardComponent />
         <EventCardComponent />
         <EventCardComponent />
