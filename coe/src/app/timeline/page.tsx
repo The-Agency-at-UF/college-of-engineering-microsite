8efeac41f30a5f24efdb12b2baaa8c2f92e82f1a
Adora Lin <157913762+LeviGotBoom@users.noreply.github.com>
2025-11-17 13:18:28 -0500

    feat: added event components and timeline onto main page (#13)
    - combined my components with main page
    - fixed spacing between event cards and their shadows are more prominent

--- coe/src/app/timeline/page.tsx
+++ coe/src/app/timeline/page.tsx
@@ -2,6 +2,8 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import TimelineHeader from "../components/TimelineHeader";
 import ExcellenceSection from "../components/ExcellenceSection";
+import Timeline from "../components/TimelineComponent";
+import EventGridComponent from "../components/EventGridComponent";
 
 export default function Home() {
   return (
@@ -15,6 +17,13 @@ export default function Home() {
       <section className="bg-white">
       <ExcellenceSection />
       </section>
+
+      {/* ======= TIMELINE BAR + EVENT GRID ======= */}
+      <section className="bg-white">
+        <Timeline />
+        <EventGridComponent />
+      </section>
+
       <Footer />
     </div>
   );
