b751b5521a7b02d76dd4847e89132b61bfd3e65a
Zachary Tenn Yuk <132629419+zacharytennyuk@users.noreply.github.com>
2025-11-18 21:30:06 -0500

    feat: rendered events and milestones from AWS (#15)
    * feat: rendered events from AWS
    
    * feat: rendered events from AWS"
    "
    g
    
    * feat: rendered milestones from AWS

--- coe/next.config.ts
+++ coe/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: [
+      "coe-milestones-bucket.s3.us-east-1.amazonaws.com", // your S3 bucket
+    ],
+  },
 };
 
 export default nextConfig;
